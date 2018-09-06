3a985bb97ce353bf1b242f9ebf5302419eec4f8e
Andrei <andrey.diving@gmail.com>
2018-09-06 22:11:32 +0500

    added grid for maincomponent

--- src/components/MapMain.js
+++ src/components/MapMain.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import GoogleMap from './google_map'
 import LogBook from './log_book/LogBook'
+import Grid from '@material-ui/core/Grid'
 
 const MapMain = () => {
   return (
-    <div>
-      <GoogleMap />
-      <LogBook />
-    </div>
+    <Grid container direction="row"  justify="center" alignItems="center">
+      <Grid item xs={9} >
+        <GoogleMap />
+      </Grid>
+      <Grid item xs={3} >
+        <LogBook />
+      </Grid>
+    </Grid>
   );
 };
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,24 @@ import {Link} from 'react-router-dom'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+
 
 const NavBar = () => {
+
     return(
         <div>
             <AppBar position="static">
                 <Toolbar>
+                    <IconButton color="inherit" aria-label="Menu">
+                      <MenuIcon />
+                    </IconButton>
                     <Typography variant="title" color="inherit">
-                      Dive Maps
-                      <Link to="/">Home</Link>
-                      <Link to="/diveSites">Divesites</Link>
-                      <Link to="/createDivesite">Create Dive Site</Link>
+                      <Button><Link to="/">Dive Maps</Link></Button>
+                      <Button><Link to="/diveSites">Divesites</Link></Button>
+                      <Button><Link to="/createDivesite">Create Dive Site</Link></Button>
                     </Typography>
                 </Toolbar>
             </AppBar>
